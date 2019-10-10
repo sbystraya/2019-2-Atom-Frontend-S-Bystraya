c4141b0ff37b34fdf0ec71da78088e4f29baeaed
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-10 15:44:09 +0000

    Deploy sbystraya/2019-2-Atom-Frontend-S-Bystraya to github.com/sbystraya/2019-2-Atom-Frontend-S-Bystraya.git:gh-pages

---
+++ precache-manifest.203729f5579af1a39cd1e7ffd027af5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e70b5de347d4d801636ae2e77cd37d7",
+    "url": "/2019-2-Atom-Frontend-S-Bystraya/index.html"
+  },
+  {
+    "revision": "5e9f202cd82459f1fe54",
+    "url": "/2019-2-Atom-Frontend-S-Bystraya/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "06691d5ba5cb69e4f1ac",
+    "url": "/2019-2-Atom-Frontend-S-Bystraya/static/js/2.cf18af9f.chunk.js"
+  },
+  {
+    "revision": "5e9f202cd82459f1fe54",
+    "url": "/2019-2-Atom-Frontend-S-Bystraya/static/js/main.303b06a2.chunk.js"
+  },
+  {
+    "revision": "1f8ebd2a8ddbed016e4a",
+    "url": "/2019-2-Atom-Frontend-S-Bystraya/static/js/runtime-main.214cc56c.js"
+  },
+  {
+    "revision": "a55f1c4358068374afaef45a15ab0eb8",
+    "url": "/2019-2-Atom-Frontend-S-Bystraya/static/media/logo.a55f1c43.svg"
+  }
+]);
